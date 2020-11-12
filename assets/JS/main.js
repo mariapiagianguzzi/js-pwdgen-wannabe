b4ecc737bb9a25a3cf20c1783e51c18be93d23ed
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2020-11-12 14:39:40 +0100

    JavaScript nome cognome colore

---
+++ assets/JS/main.js
@@ -0,0 +1,13 @@
+//Nome Cognome Colore Preferito
+
+//NOME
+var name = prompt("Come ti chiami?");
+
+//COGNOME
+var surname = prompt("Qual è il tuo conogome?");
+
+//COLORE PREFERITO
+var color = prompt("Colore preferito?");
+
+document.getElementById('password').innerHTML = name + surname + color + "19";
+
